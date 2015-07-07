ba9f548d67d4310b57f4a4550a76530ff182baa2
John Gully <john@gully.us>
2015-07-06 23:17:33 -0500

    Refactored patternPackName out of the grunt options

--- tasks/patternpack.js
+++ tasks/patternpack.js
@@ -5,6 +5,8 @@ module.exports = function (grunt) {
   var _ = require("lodash");
   _.defaultsDeep = require("merge-defaults"); // Add deep defaults capabilities to lodash
 
+  var packageName = "patternpack";
+  var packagePath = "./node_modules/" + packageName;
   var gruntTaskName = "patternpack";
   var gruntTaskDescription = "Creates a pattern library from structured markdown and styles.";
   var optionDefaults = {
@@ -28,47 +30,47 @@ module.exports = function (grunt) {
       { name: "Atoms", path: "atoms" },
       { name: "Molecules", path: "molecules" },
       { name: "Pages", path: "pages" }
-    ],
-    patternPackName: "patternpack"
+    ]
   };
+
+
 
   function setupOptions(optionOverrides) {
     var path = require("path");
-    var fs = require("fs");
 
     // Override the defaults with any user specified options
     var options = _.defaultsDeep(_.cloneDeep(optionOverrides), optionDefaults);
 
-    // Use the patternPackName to figure out the path, and ensure it exists
-    // TODO: Figure out how to abstract this path creation.  It is also used in the gruntRunner.js
-    options.packagePath = "./node_modules/" + options.patternPackName;
-    if (!fs.existsSync(options.packagePath)) {
-      throw new Error("The path to the pattern pack dependency does not exists at: " + options.packagePath);
-    }
-
     // Add the relative path to the root of the calling pattern library
-    options.root = path.relative(options.packagePath, "");
+    options.root = path.relative(packagePath, "");
 
     // Massage any paths to be relative to the child process
-    options.release = path.relative(options.packagePath, options.release);
-    options.build = path.relative(options.packagePath, options.build);
-    options.src = path.relative(options.packagePath, options.src);
-    options.assets = path.relative(options.packagePath, options.assets);
+    options.release = path.relative(packagePath, options.release);
+    options.build = path.relative(packagePath, options.build);
+    options.src = path.relative(packagePath, options.src);
+    options.assets = path.relative(packagePath, options.assets);
     // If the pattern is specified by the user then get the relative path,
     // otherwise use the path inside pattern pack to provide the default patterns.
-    options.theme = optionOverrides.theme ? path.relative(options.packagePath, options.theme) : optionDefaults.theme;
+    options.theme = optionOverrides.theme ? path.relative(packagePath, options.theme) : optionDefaults.theme;
 
     return options;
   }
 
   function saveOptions(options) {
-    var file = options.packagePath + "/gruntfileConfig.json";
+    var file = packagePath + "/gruntfileConfig.json";
     var contents = JSON.stringify(options);
     grunt.file.write(file, contents);
   }
 
   function gruntPatternPackTask() {
     var done = this.async();
+
+    // Ensure that the packagePath exists.
+    // TODO: Figure out how to abstract this path creation.  It is also used in the gruntRunner.js
+    var fs = require("fs");
+    if (!fs.existsSync(packagePath)) {
+      throw new Error("The path to the pattern pack dependency does not exists at: " + packagePath);
+    }
 
     // Get the options
     var options = setupOptions(this.options());
@@ -84,7 +86,7 @@ module.exports = function (grunt) {
     // Create the options required to run the child grunt process
     var gruntRunner = require("../gruntRunner.js")(grunt);
     var gruntRunnerOptions = {
-      name: options.patternPackName,
+      name: packageName,
       tasks: options.task,
       flags: grunt.option.flags()
     };
